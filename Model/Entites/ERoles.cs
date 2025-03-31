b63cba9959a47674be585d0eaba154795a8d6608
werwolfy <raphael.nigl@outlook.de>
2025-03-31 08:44:00 +0200

    [TASK] Added User.cs

---
+++ Model/Entites/ERoles.cs
@@ -0,0 +1,7 @@
+﻿namespace Model.Entites;
+
+public enum ERoles
+{
+    Admin = 0,
+    Customer = 1
+}
---
+++ Model/Entites/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Model.Entites;
+
+[Table("USERS")]
+public class User
+{
+    [Column("USER_ID"), Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int UserId { get; set; }
+    [Column("USER_MAIL"), Required, MaxLength(320)]
+    public string? Email { get; set; }
+    [Column("PASSWORD_HASH"), Required, MaxLength(100)]
+    public string? PasswordHash { get; set; }
+    [Column("ADDRESS"), Required, MaxLength(50)]
+    public string? Address { get; set; }
+    [Column("ROLE"), Required] 
+    public ERoles Role { get; set; }
+}
