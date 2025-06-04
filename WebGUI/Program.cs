9123cc568aee0eab9b1bf566701fd6e857b08f33
riegc <c.rieger@htlkrems.at>
2025-06-04 14:58:19 +0200

    changed type to supplementtype

--- WebGUI/Program.cs
+++ WebGUI/Program.cs
@@ -89,9 +89,9 @@ else
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseAntiforgery();
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseAntiforgery();
 
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
